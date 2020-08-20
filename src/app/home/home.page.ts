09c24582fb903756b4e5cbf6566b7e2dfabb7fe2
Adrian Vatchinsky <adrian@uni.fyi>
2020-08-19 21:20:40 -0400

    figured out how to plot a curve through many points

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,7 +10,7 @@ import { Component, OnInit, AfterViewInit, OnDestroy } from "@angular/core";
 export class HomePage implements OnInit, OnDestroy, AfterViewInit {
   // ------------------------
 
-  private p5;
+  private p5: any;
 
   // ------------------------
 
@@ -27,18 +27,41 @@ export class HomePage implements OnInit, OnDestroy, AfterViewInit {
   // ------------------------
 
   private createCanvas() {
-    this.p5 = new p5(this.sketch);
-  }
+    this.p5 = new p5((sketch) => {
+      let coords = [40, 40, 80, 60, 100, 100, 60, 120, 50, 150];
+      let song;
 
-  private sketch(p: any) {
-    p.setup = () => {
-      p.createCanvas(256, 256).parent("sketch-canvas");
-    };
+      sketch.setup = () => {
+        sketch.createCanvas(256, 256).parent("sketch-canvas");
 
-    p.draw = () => {
-      p.background(255);
-      p.fill(0);
-      p.rect(p.width / 2, p.height / 2, 50, 50);
-    };
+        // sketch.soundFormats("mp3");
+        // song = p.loadSound("assets/Kenney_Floreat_-_02_-_Crucify.mp3");
+      };
+
+      sketch.loaded = () => {
+        // song.play();
+      };
+
+      sketch.draw = () => {
+        // sketch.background(255);
+        // sketch.fill(0);
+        // sketch.rect(sketch.width / 2, sketch.height / 2, 50, 50);
+
+        sketch.background(255);
+        sketch.noFill();
+        sketch.stroke(0);
+        sketch.beginShape();
+        sketch.curveVertex(40, 40);
+        sketch.curveVertex(40, 40);
+        sketch.curveVertex(80, 60);
+        sketch.curveVertex(100, 100);
+        sketch.curveVertex(60, 120);
+        sketch.curveVertex(50, 150);
+        sketch.curveVertex(50, 150);
+        sketch.endShape();
+
+        // once FFT is done, connect w https://p5js.org/learn/curves.html
+      };
+    });
   }
 }
