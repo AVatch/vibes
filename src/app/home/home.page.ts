bcd7e9644bf30ac39022e40784d7e376eae80497
Adrian Vatchinsky <adrian@uni.fyi>
2020-08-19 20:26:50 -0400

    added p5 dep + got it working w ng

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import * as p5 from "p5";
+
+import { Component, OnInit, AfterViewInit, OnDestroy } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"],
 })
-export class HomePage {
+export class HomePage implements OnInit, OnDestroy, AfterViewInit {
+  // ------------------------
+
+  private p5;
+
+  // ------------------------
 
   constructor() {}
 
+  ngOnInit() {}
+
+  ngOnDestroy() {}
+
+  ngAfterViewInit() {
+    this.createCanvas();
+  }
+
+  // ------------------------
+
+  private createCanvas() {
+    this.p5 = new p5(this.sketch);
+  }
+
+  private sketch(p: any) {
+    p.setup = () => {
+      p.createCanvas(256, 256).parent("sketch-canvas");
+    };
+
+    p.draw = () => {
+      p.background(255);
+      p.fill(0);
+      p.rect(p.width / 2, p.height / 2, 50, 50);
+    };
+  }
 }
